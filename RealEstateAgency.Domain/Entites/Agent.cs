5ce0f71917d42097c6296dd8b8478594a7dee559
Judori <v.steblev02@gmail.com>
2022-01-18 19:39:36 +0300

    Добавил сущности Agent и Client

---
+++ RealEstateAgency.Domain/Entites/Agent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateAgency.Domain.Entites
+{
+    public class Agent
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public int DealShare { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
---
+++ RealEstateAgency.Domain/Entites/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateAgency.Domain.Entites
+{
+    public class Client
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
