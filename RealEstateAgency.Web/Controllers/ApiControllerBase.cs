5a2a710f0e815d45568a6ba10da26a7e67016af4
Judori <v.steblev02@gmail.com>
2022-01-19 19:37:44 +0300

    Работа с контроллерами

---
+++ RealEstateAgency.Web/Controllers/ApiControllerBase.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateAgency.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ApiControllerBase : ControllerBase
+    {
+        private ISender _mediatr = null!;
+
+        protected ISender Mediator => _mediatr ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
---
+++ RealEstateAgency.Web/Controllers/ClientListController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateAgency.Application.Clients.Queries.GetClientList;
+
+namespace RealEstateAgency.Web.Controllers
+{
+    public class ClientListController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<ClientListVm>> GetAll()
+        {
+            var query = new GetClientListQuery();
+            var vm = await Mediator.Send(query);
+
+            return Ok(vm);
+        }
+    }
+}
